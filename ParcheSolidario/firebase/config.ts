fc38cd9abb78320940dbd2b71ecfbcb706dcf380
sebasdj2006@gmail.com <sebasdj2006@gmail.com>
2026-03-07 10:28:55 -0500

    base de datos new

--- ParcheSolidario/firebase/config.ts
+++ ParcheSolidario/firebase/config.ts
@@ -1,9 +1,11 @@
 // Configuración de Firebase - Next.js carga automáticamente las variables de entorno
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBhCxT5IhaCpxOa6GHkcrJr5MCjDJj8svU",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "parchesolidario-d1d9c.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "parchesolidario-d1d9c",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "parchesolidario-d1d9c.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "464510586595",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:464510586595:web:8d28070a336aeabc1dffd6",
+    apiKey: "AIzaSyA25D2uOxU30_MSngYd7eorkLRIRkfNQus",
+    authDomain: "parche-solidario-v2.firebaseapp.com",
+    projectId: "parche-solidario-v2",
+    storageBucket: "parche-solidario-v2.firebasestorage.app",
+    messagingSenderId: "380153434603",
+    appId: "1:380153434603:web:dec950095ae399f8dc89ae",
+    measurementId: "G-LNRW2ECBVT"
+  
 };
